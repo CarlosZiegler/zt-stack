fabdfdbcef0b4b43d8ab92a868d05bd59bd1ee71
carlosziegler <carlos.ziegler@gmail.com>
2025-03-09 20:14:26 +0100

    feat: move files to avoid lazy files

--- apps/web/src/routes/_protected/admin/index.tsx
+++ apps/web/src/routes/_protected/admin/index.tsx
@@ -1,7 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router';
+
 import { queryClient } from '@/clients/queryClient';
 import { trpc } from '@/router';
 
+import AdminDashboard from './-components/admin-dashboard';
+
 export const Route = createFileRoute('/_protected/admin/')({
   loader: () => queryClient.ensureQueryData(trpc.posts.all.queryOptions()),
+
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  return <AdminDashboard />;
+}
--- apps/web/src/routes/_protected/posts/$postid/index.tsx
+++ apps/web/src/routes/_protected/posts/$postid/index.tsx
@@ -39,4 +39,39 @@ export const Route = createFileRoute('/_protected/posts/$postid/')({
       </div>
     );
   },
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  const { t } = useTranslation();
+  const post = Route.useLoaderData();
+
+  return (
+    <div className='flex flex-col px-4 w-full max-w-6xl mx-auto break-words'>
+      <div className='text-center p-5 rounded-2xl'>
+        <h1 className='text-2xl md:text-4xl font-bold'>{post.title}</h1>
+        <p className='text-sm text-gray-500 mt-2'>
+          {t('CREATED_BY')}{' '}
+          <span className='font-medium'>{post.author.name}</span>,{' '}
+          {post.createdAt.toLocaleString()}
+        </p>
+      </div>
+      <hr className='border border-gray-500 mt-3' />
+      <Button
+        asChild
+        variant='link'
+        className='w-12 border border-gray-500 mt-6'
+      >
+        <Link {...postsLinkOptions}>
+          <ArrowLeftIcon />
+        </Link>
+      </Button>
+
+      <div className='bg-elevated shadow rounded-2xl p-6 w-full min-h-96 border border-gray-500 break-words mt-6'>
+        <p className='leading-relaxed whitespace-break-spaces'>
+          {post.content ?? t('NO_CONTENT_AVAILABLE')}
+        </p>
+      </div>
+    </div>
+  );
+}
